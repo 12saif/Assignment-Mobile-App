8624867d41c9f4e49b48440b61645aaef13c3294
MacBook Air <macbookair@MacBooks-MacBook-Air.local>
2023-12-09 17:05:48 +0530

    initial commit

--- Screens/HomeScreen/Home.js
+++ Screens/HomeScreen/Home.js
@@ -1,10 +1,14 @@
-import { View, Text, SafeAreaView, ScrollView, Image, TouchableOpacity, Alert } from "react-native";
-import React, { useLayoutEffect } from "react";
-import { useNavigation } from "@react-navigation/native";
-import SectionTop from "./SectionTop";
-import ScrollCards from "./ScrollCards";
-import OurPremiumService from "./OurPremiumService";
-import OurService from "./OurService";
+import { StyleSheet, Text, View } from 'react-native'
+import React,{useLayoutEffect} from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { useNavigation } from '@react-navigation/native';
+
+const data=[
+  {
+		tag: "Relationship",
+		text: "want to I love you to my wife and kids "
+	},
+]
 
 const Home = () => {
   const Navigation = useNavigation();
@@ -15,33 +19,20 @@ const Home = () => {
     });
   }, []);
   return (
-    <SafeAreaView style={{ marginTop: 30 }} >
-      <ScrollView showsVerticalScrollIndicator={false} style={{ backgroundColor: '#FFFFFF' }} >
-        <View style={{ flex: 1, flexDirection: 'row', justifyContent: 'space-between', marginHorizontal: 20, marginVertical: 20 }}>
-          <View style={{ flex: 1, flexDirection: 'row', alignItems: 'center' }}>
-            <TouchableOpacity onPress={() => Alert.alert('This is sidebar', 'On iOS you can specify any number of buttons. Each button can optionally specify a style ')}>
-              <Image source={require('./assets/Icon.png')} style={{ width: 30, height: 30, marginRight: 20 }} />
-            </TouchableOpacity>
-            <Image source={require('./assets/Frame8552.png')} style={{ width: 30, height: 30, marginRight: 5 }} />
-            <Text style={{ fontSize: 19, fontWeight: 800 }}>Right</Text>
-            <Text style={{ fontSize: 17 }}>Astrology</Text>
-          </View>
-          <View>
-            <TouchableOpacity style={{ borderWidth: 1, borderColor: '#525FE1', borderRadius: 50, paddingHorizontal: 20, paddingVertical: 10 }} >
-              <View style={{ flexDirection: 'row', alignItems: 'center' }} >
-                <Text style={{ marginRight: 15, color: '#FF6000' }} >1,000</Text>
-                <Image source={require('./assets/Iconwellet.png')} style={{ width: 30, height: 30 }} />
-              </View>
-            </TouchableOpacity>
-          </View>
-        </View>
-        <SectionTop />
-        <ScrollCards />
-        <OurPremiumService />
-        <OurService />
-      </ScrollView>
+    <SafeAreaView>
+      <Text style={{fontSize:26, fontWeight:400,marginLeft:16,marginBottom:10}}>Suggestion</Text>
+      {data.map((item) => {
+      <View style={{height:81,padding:20,backgroundColor:'#DEF6FB',justifyContent:'center',alignItems:'center',marginVertical:10}}>
+      <Text style={{fontSize:16,position:'absolute',top:0,right:0,backgroundColor:'#00D3FF',padding:4}}>{item.tag}</Text>
+        <Text style={{fontSize:16}}>
+           want to I love you to my wife and kids 
+        </Text>
+      </View>
+      })}
     </SafeAreaView>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
+
+const styles = StyleSheet.create({})
---
+++ Screens/ProfileScreen/Profile.js
@@ -0,0 +1,23 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React,{useLayoutEffect} from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { useNavigation } from '@react-navigation/native';
+
+const Profile = () => {
+  const Navigation = useNavigation();
+
+  useLayoutEffect(() => {
+    Navigation.setOptions({
+      headerShown: false,
+    });
+  }, []);
+  return (
+    <SafeAreaView>
+      <Text>Profile</Text>
+    </SafeAreaView>
+  )
+}
+
+export default Profile
+
+const styles = StyleSheet.create({})
